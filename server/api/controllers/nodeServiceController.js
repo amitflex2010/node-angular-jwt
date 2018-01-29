bb2b212d3b7a86c0112167e85b06def4960274cf
Amit Tripathi <amit.tripathi@klm.com>
2018-01-29 17:16:57 +0100

    imgae upload

--- server/api/controllers/nodeServiceController.js
+++ server/api/controllers/nodeServiceController.js
@@ -127,11 +127,12 @@ res.json(register_task);
 
 var storage = multer.diskStorage({ //multers disk storage settings
     destination: function (req, file, cb) {
-        cb(null, './uploads/');
+        cb(null, '../src/uploads/');
     },
     filename: function (req, file, cb) {
         var datetimestamp = Date.now();
-        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+        //cb(null, file.originalname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+        cb(null, file.originalname);
     }
 });
 
